bf504a31bf9bd13b97210a5d19f52036b76fb4a8
thangqd <quachdongthang@gmail.com>
2024-10-10 14:30:40 +0700

    update test

--- vgrid/utils/geocode/test.py
+++ vgrid/utils/geocode/test.py
@@ -1,6 +1,25 @@
-from vgrid.utils.geocode import mgrs, maidenhead, geohash, olc
+from vgrid.utils.geocode import mgrs, maidenhead, geohash, olc, georef
+from vgrid.vcode import vcode
 latitude, longitude = 10.775275567242561, 106.70679737574993
 print(f'latitude, longitude: ({latitude}, {longitude})')
+
+
+# OLC
+print('\nOpen Location Code (OLC):')
+olc_precision = 10
+olc_code = olc.encode(latitude, longitude, olc_precision)
+olc_decode = olc.decode(olc_code)
+print(f'OLC at precision = {olc_precision}: {olc_code}')
+print(f'Decode {olc_code} to center and cell in WGS84 = {olc_decode}')
+
+# Vcode
+print('\nVcode:')
+vcode_zoom = 25
+latlon2vcode = vcode.latlon2vcode(latitude, longitude, vcode_zoom)
+vcode2latlon = vcode.vcode2latlon(latlon2vcode)
+print(f'Vcode at zoom level = {vcode_zoom}: {latlon2vcode}')
+print(f'Convert {latlon2vcode} back to WGS84 = {vcode2latlon}')
+
 
 # MGRS
 print('\nMGRS:')
@@ -24,12 +43,12 @@ geohash_precision = 12
 geohash_code = geohash.encode(latitude, longitude, geohash_precision)
 geohash_decode = geohash.decode(geohash_code, False)
 print(f'Geohash Code at precision = {geohash_precision}: {geohash_code}')
-print(f'Decode {geohash_code} to center in WGS84 = {geohash_decode}')
+print(f'Decode {geohash_code} to WGS84 = {geohash_decode}')
 
-# OLC
-print('\nOpen Location Code (OLC):')
-olc_precision = 10
-olc_code = olc.encode(latitude, longitude, olc_precision)
-olc_decode = olc.decode(olc_code)
-print(f'OLC at precision = {olc_precision}: {olc_code}')
-print(f'Decode {olc_code} to center and cell in WGS84 = {olc_decode}')
+# GEOREF
+print('\nGEOREF:')
+georef_precision = 10
+georef_code = georef.encode(latitude, longitude, georef_precision)
+georef_decode = georef.decode(georef_code, False)
+print(f'GEOREF Code at precision = {georef_precision}: {georef_code}')
+print(f'Decode {georef_code} to WGS84 = {georef_decode}')
