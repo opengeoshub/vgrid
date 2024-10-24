27670077eff54d5e49462af4dfc5a1158a3da6a1
thangqd <quachdongthang@gmail.com>
2024-10-24 16:31:16 +0700

    Update h3_test.py

--- vgrid/test/h3_test.py
+++ vgrid/test/h3_test.py
@@ -3,7 +3,7 @@ from vgrid.geocode.geocode2geojson import *
 
 latitude, longitude = 10.775275567242561, 106.70679737574993
 
-h3_precision = 13
+h3_precision = 13 #(0-15)
 h3_code = h3.latlng_to_cell(latitude, longitude, h3_precision)
 h3_decode = h3.cell_to_latlng(h3_code)
 
