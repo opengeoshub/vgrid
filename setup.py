ab300460fd47636e3d5c926490843f80b1647313
thangqd <quachdongthang@gmail.com>
2024-11-05 11:25:43 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ clean_build()
 
 setup(
     name='vgrid',
-    version='1.1.2',
+    version='1.1.3',
     author = 'Thang Quach',
     author_email= 'quachdongthang@gmail.com',
     url='https://github.com/thangqd/vgrid',
